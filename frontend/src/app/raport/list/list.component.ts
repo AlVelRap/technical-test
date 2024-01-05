b51a41423f31adcba76b8113ad79d5eb1965c6df
Alberto Velázquez Rapado <86166779+AlVelRap@users.noreply.github.com>
2024-01-05 05:29:56 +0100

    feat: fetch list of players for the list

--- frontend/src/app/raport/list/list.component.ts
+++ frontend/src/app/raport/list/list.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Player } from '../../models/player.model';
+import { PlayerService } from '../../services/player.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrl: './list.component.scss'
+  styleUrl: './list.component.scss',
 })
 export class ListComponent {
+  playerList: Player[] = [];
 
+  constructor(private playerService: PlayerService) {}
+
+  ngOnInit(): void {
+    this.playerService.getAll().subscribe({
+      next: (response) => {
+        this.playerList = response;
+        console.log(response);
+      },
+      error: (errorData) => {
+        console.error(errorData);
+      },
+      complete: () => {
+        console.info('Carga completa');
+      },
+    });
+  }
 }
