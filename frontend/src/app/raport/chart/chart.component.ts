7ceab6b99bbb4bcec6f2a4ac8022e1769a39d960
Alberto Velázquez Rapado <86166779+AlVelRap@users.noreply.github.com>
2024-01-05 06:03:22 +0100

    feat: get fatigue data from player

--- frontend/src/app/raport/chart/chart.component.ts
+++ frontend/src/app/raport/chart/chart.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Fatigue } from '../../models/fatigue.model';
+import { FatigueService } from '../../services/fatigue.service';
+import {
+  ActivatedRoute,
+  Event,
+  NavigationStart,
+  Router,
+} from '@angular/router';
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
-  styleUrl: './chart.component.scss'
+  styleUrl: './chart.component.scss',
 })
-export class ChartComponent {
+export class ChartComponent implements OnInit {
+  fatigueData!: Fatigue[];
+  id_player!: string;
+  chartError: string = '';
 
+  constructor(
+    private fatigueService: FatigueService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.route.params.subscribe((parameter) => {
+      this.id_player = parameter['id_player'];
+      this.fatigueService.findByPlayer(this.id_player).subscribe({
+        next: (data) => {
+          console.log(data);
+          this.fatigueData = data;
+        },
+        error: (errorData) => {
+          this.chartError = "Jugador sin datos de fatiga registrados.";
+        },
+      });
+    });
+  }
+
+  ngOnInit(): void {
+    this.fatigueService.findByPlayer(this.id_player).subscribe({
+      next: (data) => {
+        console.log(data);
+        this.fatigueData = data;
+      },
+    });
+  }
+  // get id_player() {
+  //   return this.route.snapshot.paramMap.get('id_player') || '';
+  // }
 }
