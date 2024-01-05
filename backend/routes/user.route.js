856dc17ba5ecaac0f9da70aa1438303bb154a5f7
Alberto Velázquez Rapado <86166779+AlVelRap@users.noreply.github.com>
2024-01-05 04:58:00 +0100

    feat: remove register route from backend

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -4,7 +4,6 @@ module.exports = (app) => {
   const user = require("../controllers/user.controller");
   var router = require("express").Router();
 
-  router.post("/register", user.register);
   router.post("/login", user.login);
 
   app.use(`/${API}/${REST}/${VERSION}/user`, router);
